3e324e3c7eddc9d904a901abbac27d8fec53efab
TomtomFH <bfc.officialyt@gmail.com>
2025-05-01 18:20:47 +0200

    changed uti

--- 105344022671234.lua
+++ 105344022671234.lua
@@ -12,6 +12,7 @@ CreateTab("UTI", "Main", "Farming")
 CreateToggle("Farming", "Auto Infinite Farm Range", function(state)
     while state.Value do
         character.Detector.Size = Vector3.new(1, 2048, 2048)
+        character.Detector.Circle.Enabled = false
         task.wait(1)
     end
 end)
